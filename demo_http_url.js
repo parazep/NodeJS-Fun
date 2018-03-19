2e443b7eb4103cde8661a1cfbf04d9198daa51c0
Pete <parazep@gmail.com>
2018-03-19 14:53:28 +0000

    New Files

---
+++ demo_http_url.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+}).listen(8080);
